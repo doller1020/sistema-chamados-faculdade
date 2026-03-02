b9dc91ec25b8d3d169d420190d88a4a24ef51511
Doller Felipe Macedo Costa <dollerfelipe10@gmail.com>
2026-03-02 18:38:40 -0300

    Menu inicial funcional do sistema de chamados

---
+++ app.py
@@ -0,0 +1,22 @@
+
+print("================================")
+print(" Sistema de Chamados - Faculdade")
+print("================================")
+
+print("1 - Abrir chamado")
+print("2 - Sair")
+
+opcao = input("Escolha uma opção: ")
+
+if opcao == "1":
+    descricao = input("Descreva o problema: ")
+    print("Chamado aberto com sucesso!")
+    print("Descrição:", descricao)
+
+elif opcao == "2":
+    print("Encerrando o sistema...")
+
+else:
+    print("Opção inválida")
+
+
